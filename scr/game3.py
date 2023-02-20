2273f331caa9eff9216c879592b24a03afae7e10
GiaBab <gabrielbianglino@gmail.com>
2023-02-19 23:47:29 -0300

    update game3 and menu

--- scr/game3.py
+++ scr/game3.py
@@ -7,29 +7,32 @@ class WhoHaveMoreItems():
         self.monoky3=Monoky()
 
     def __repr__(self):
-        return f"monky1{self.monoky1.getArray()} monky2{self.monoky2.getArray()} monky3{self.monoky3.getArray()}"
+        return f"monky1{self.monoky1.get()} monky2{self.monoky2.get()} monky3{self.monoky3.get()}"
 
     def play(self):
+        print("You have 3 monoky in lab, monoky have items what is going to pass and you must do guess who monoky have more items.")
         i=0
         while i < 5:
             self.monoky1.send(self.monoky2,self.monoky3,5)
             self.monoky2.send(self.monoky1,self.monoky3,3)
             self.monoky3.send(self.monoky2,self.monoky1,2)
             i+=1
+        self.winCondition()
 
-    def len(self):
+    def allLen(self):
         return [self.monoky1.len(), self.monoky2.len(), self.monoky3.len()]
     
     def whoHaveMoreItems(self):
-        for i in range(self.len()):
-            if self.len()[i] == max(self.len()) :
+        for i in range(len(self.allLen())):
+            if self.allLen()[i] == max(self.allLen()) :
                 return i
 
     def winCondition(self):
         num = int(input()) 
         if self.whoHaveMoreItems() == num :
             print("you win :)")
-
+        else :
+            print("you lose :(")
 
 class Monoky():
     def __init__(self) -> None:
@@ -37,19 +40,30 @@ class Monoky():
         ds.generateRandomArray(self.items, 2, 5)
 
     def append(self, num:int):
-        return self.getArray().append(num)
+        return self.items.append(num)
 
     def send(self, monoky1, monoky2, num):
-        if self.getArray().get():
+        if self.items.get():
             aux = self.pop()
             if aux%num==0 :
                 monoky1.append((aux//3)*(1+num))
             else:
                 monoky2.append((aux//2)*(num-1))
 
+    def get(self):
+        return self.items.get()
+
     def pop(self):
-        return self.getArray().pop()
+        return self.items.pop()
 
     def len(self):
         return self.items.len()
 
+def testing():
+    gTest = WhoHaveMoreItems()
+    print(gTest)
+    gTest.play()
+    print(gTest)
+    print(gTest.allLen())
+
+testing()
--- scr/menu.py
+++ scr/menu.py
@@ -1,22 +1,21 @@
-import scr.game1 as g1, scr.game2 as g2
+import scr.game1 as g1, scr.game2 as g2, scr.game3 as g3
 
 class MenuGame:
     def __init__(self):
-        self.opc = ["RockPaperScissor", "RandomNumber"]
-    
-    def games(self):
-        item1 = g1.RockPaperSiccors()
-        item2 = g2.RandomNumers(0, 10)
-        return item1, item2
+        self.opc = ["RockPaperScissor", "RandomNumber", "WhoHaveMoreItems"]
+        self.item1 = g1.RockPaperSiccors()
+        self.item2 = g2.RandomNumers(0, 10)
+        self.item3 = g3.WhoHaveMoreItems()
 
     def play(self):
         self.opcions()
         opc = int(input("input: "))
-        item1, item2 = self.games()
         match opc :
-            case 1 : item1.play()
+            case 1 : self.item1.play()
 
-            case 2 : item2.play()
+            case 2 : self.item2.play()
+
+            case 3 : self.item3.play()
 
     def opcions(self):
         [print(f"[{i+1}]-{opc}") for i, opc in enumerate(self.opc)]
